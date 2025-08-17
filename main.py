9524348b0fe44819d9f31c5ebde49537fd1d9a93
growlithe2013 <eeveenoises@gmail.com>
2025-08-17 01:13:29 -0700

    added quitting

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-import pygame
+import pygame # type: ignore
 from constants import *
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        pass
 
 if __name__ == "__main__":
     main()
