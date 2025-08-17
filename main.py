03f84857ab9f4e91cb05ec08e69bc472baf8fb20
growlithe2013 <eeveenoises@gmail.com>
2025-08-17 01:52:52 -0700

    triangle drawn to screen

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame # type: ignore
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     print("Starting Asteroids!")
@@ -7,9 +9,23 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt=0
+
+    player1 = player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
-        pass
+
+        screen.fill("black")
+
+        player1.draw(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.display.flip()
+        dt=clock.tick(60)/1000
+        
 
 if __name__ == "__main__":
     main()
